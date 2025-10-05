92e0a10bdfcaa1e3c391609c5d24a5504ce82391
sizweazora-ai <sizwe.azora@gmail.com>
2025-10-05 07:42:47 +0200

    Create index.ts

---
+++ src/atomic/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Barrel file for the atomic directory.
+ * This simplifies imports for organisms and pages.
+ */
+export * from './NotificationProvider';
+export * from './hooks/useNotify';
+export { default as NotificationToast } from './molecules/NotificationToast';
