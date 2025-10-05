e36126a1654075374c47b177ac9d149ee8f4c1f9
sizweazora-ai <sizwe.azora@gmail.com>
2025-10-05 06:40:16 +0200

    feat: add atomic LeaderboardWidget organism and subcomponents

---
+++ src/components/azora/LeaderboardWidget.tsx
@@ -0,0 +1,13 @@
+// LeaderboardWidget.tsx
+import React from 'react';
+
+const LeaderboardWidget = () => {
+    return (
+        <div>
+            <h1>Leaderboard</h1>
+            {/* Add leaderboard rows here */}
+        </div>
+    );
+};
+
+export default LeaderboardWidget;
