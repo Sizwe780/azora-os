b7025a74ea745bf15f5c6c5c6d2dfea3f43b0a59
Sizwe Ngwenya <112629001+Sizwe780@users.noreply.github.com>
2025-10-07 11:34:36 +0200

    Update DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -5,11 +5,22 @@ import ThemeToggle from "../components/azora/ThemeToggle";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex bg-slate-100 dark:bg-slate-900 relative">
+    <div className="min-h-screen flex bg-slate-100 dark:bg-slate-900 relative overflow-hidden">
+      {/* Animated background beams */}
       <BeamsBackground />
-      <ThemeToggle />
+
+      {/* Sidebar */}
       <Sidebar />
-      <main className="flex-1 p-8 flex flex-col gap-8 items-center">{children}</main>
+
+      {/* Main content */}
+      <main className="flex-1 p-8 flex flex-col gap-8 relative z-10">
+        {children}
+      </main>
+
+      {/* Floating theme toggle */}
+      <div className="fixed bottom-6 right-6 z-20">
+        <ThemeToggle />
+      </div>
     </div>
   );
 }
